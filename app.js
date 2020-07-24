c6f2d792ad2dead5262178b2ca6df01a18f22957
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2020-07-25 04:36:11 +0700

    add web ui

--- app.js
+++ app.js
@@ -1,96 +1,114 @@
 const express = require("express");
 const app = express();
-const PORT = 80;
 const irc = require("irc");
 const axios = require("axios");
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const moment = require('moment');
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const moment = require("moment");
+const exphbs = require("express-handlebars");
 
 require("dotenv").config();
 
-const { clientId, oauthToken, tmiToken } = process.env;
+var hbs = exphbs.create({
+  helpers: {
+    inc: value => parseInt(value) + 1
+  }
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+const { port, clientId, oauthToken, tmiToken, ircUsername } = process.env;
 const urlCallback = "https://d74891ac43d2.ngrok.io/twitch";
 
-const [formatFile] = ['.txt'];
+const [formatFile] = [".txt"];
 
-const dateNow = moment(new Date()).format('DD-MM-YYYY');
-const locationParent = path.join(process.cwd(), 'records');
+const dateNow = moment(new Date()).format("DD-MM-YYYY");
+const locationParent = path.join(process.cwd(), "records");
 const locationFile = path.join(locationParent, dateNow + formatFile);
+app.use(express.static(path.join(process.cwd(), "public")));
 
-let client = new irc.Client('irc.chat.twitch.tv', 'aldiwildan_', {
-  channels: ['#fl0m'],
+let client = new irc.Client("irc.chat.twitch.tv", ircUsername, {
+  channels: ["#fl0m"],
   password: tmiToken
 });
 
-client.addListener('message', function (from, to, message, data) {
+client.addListener("message", function(from, to, message, data) {
   const dataChunk = `${from}: ${message} \n`;
   fsPromises.appendFile(locationFile, dataChunk, {});
 });
 
-client.addListener('error', function (message) {
-  console.log('error: ', message);
-});
+client.addListener("error", function(message) {});
 
-axios({
-  method: "POST",
-  url: "https://api.twitch.tv/helix/webhooks/hub",
-  headers: {
-    "Client-Id": clientId,
-    Authorization: `Bearer ${oauthToken}`
-  },
-  data: {
-    "hub.callback": urlCallback,
-    "hub.mode": "subscribe",
-    "hub.topic": "https://api.twitch.tv/helix/streams?user_id=156806450",
-    "hub.lease_seconds": "50000"
-  }
-})
-  .then(res => {
-    console.log(res.status);
-  })
-  .catch(error => {
-    console.log(error.response.data);
-  });
-
-app.use(express.json());
-
-app.get("/twitch", (req, res) => {
-  console.log(req.query);
-  res.setHeader("content-type", "text/plain");
-  res.send(req.query["hub.challenge"]);
-});
-
-app.post("/twitch", (req, res) => {
-  console.log(req.body)
-  res.status(200).json({
-    message: 'ok',
+app.get("", (req, res) => {
+  res.render("index", {
+    layout: false
   });
 });
 
-app.get('/records', async (req, res) => {
+app.get("/api/data", async (req, res) => {
   const query = req.query;
 
   try {
-    if (!query) return res.status(400).json({ message: 'some params is missing' });
+    if (!query)
+      return res.status(400).json({ message: "some params is missing" });
 
-    const file = await fsPromises.readFile(path.join(locationParent, query.date + formatFile));
-    if (file.length == 0) return res.status(404).json({ message: 'file not found' });
+    if (!fs.existsSync(path.join(locationParent, query.date + formatFile))) {
+      return res.status(404).json({ message: "File not found" });
+    }
+    const file = await fsPromises.readFile(
+      path.join(locationParent, query.date + formatFile)
+    );
 
-    const fileConverted = file.toString('utf8');
-    let arrayFile = fileConverted.split('\n');
+    const fileConverted = file.toString("utf8");
+    let arrayFile = fileConverted.split("\n");
 
     res.status(200).json({ data: arrayFile });
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ message: 'Internal Server Error' })
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-app.listen(PORT, () => {
+// axios({
+//   method: "POST",
+//   url: "https://api.twitch.tv/helix/webhooks/hub",
+//   headers: {
+//     "Client-Id": clientId,
+//     Authorization: `Bearer ${oauthToken}`
+//   },
+//   data: {
+//     "hub.callback": urlCallback,
+//     "hub.mode": "subscribe",
+//     "hub.topic": "https://api.twitch.tv/helix/streams?user_id=156806450",
+//     "hub.lease_seconds": "50000"
+//   }
+// })
+//   .then(res => {
+//     console.log(res.status);
+//   })
+//   .catch(error => {
+//     console.log(error.response.data);
+//   });
+
+// app.use(express.json());
+
+// app.get("/twitch", (req, res) => {
+//   console.log(req.query);
+//   res.setHeader("content-type", "text/plain");
+//   res.send(req.query["hub.challenge"]);
+// });
+
+// app.post("/twitch", (req, res) => {
+//   console.log(req.body)
+//   res.status(200).json({
+//     message: 'ok',
+//   });
+// });
+
+app.listen(port, () => {
   if (!fs.existsSync(locationParent)) {
     fs.mkdirSync(locationParent);
   }
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
