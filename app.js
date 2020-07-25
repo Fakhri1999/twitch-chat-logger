9d5891b9fe89c695701cdb8dc8c7105ec08598b1
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2020-07-25 15:18:48 +0700

    add socketio for real time log (still need improvement)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const irc = require("irc");
 const axios = require("axios");
 const path = require("path");
@@ -26,15 +28,18 @@ const [formatFile] = [".txt"];
 const dateNow = moment(new Date()).format("DD-MM-YYYY");
 const locationParent = path.join(process.cwd(), "records");
 const locationFile = path.join(locationParent, dateNow + formatFile);
+let lastMessage = ["lastMessage"];
+let newMessage = [];
 app.use(express.static(path.join(process.cwd(), "public")));
 
 let client = new irc.Client("irc.chat.twitch.tv", ircUsername, {
-  channels: ["#fl0m"],
+  channels: ["#eryctriceps"],
   password: tmiToken
 });
 
 client.addListener("message", function(from, to, message, data) {
   const dataChunk = `${from}: ${message} \n`;
+  newMessage.push(dataChunk);
   fsPromises.appendFile(locationFile, dataChunk, {});
 });
 
@@ -51,7 +56,7 @@ app.get("/api/data", async (req, res) => {
 
   try {
     if (!query)
-      return res.status(400).json({ message: "some params is missing" });
+      return res.status(400).json({ message: "Some params is missing" });
 
     if (!fs.existsSync(path.join(locationParent, query.date + formatFile))) {
       return res.status(404).json({ message: "File not found" });
@@ -68,6 +73,40 @@ app.get("/api/data", async (req, res) => {
     console.error(error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+let clientConnected = 0;
+io.on("connection", socket => {
+  console.log("New connection established");
+  clientConnected++;
+  console.log(`Client : ${clientConnected}`);
+
+  socket.on("setDate", data => {
+    console.log(data);
+    let emitData = {
+      clientId: clientConnected,
+      date: data
+    };
+    socket.emit("setStatus", emitData);
+  });  
+  setInterval(() => { 
+    console.clear()
+    console.log(`lastMessage : ${lastMessage}`);
+    console.log(`newMessage : ${newMessage}`);
+    
+    if(!(lastMessage == newMessage) && newMessage.length > 0) {
+      console.log("Masuk sini");      
+      socket.emit(dateNow, newMessage)
+      lastMessage = newMessage
+      newMessage = []
+    }
+  }, 1000);
+
+  socket.on("disconnect", () => {
+    console.log("Connection disconnected\n");
+
+    clientConnected--;
+  });
 });
 
 // axios({
@@ -106,7 +145,7 @@ app.get("/api/data", async (req, res) => {
 //   });
 // });
 
-app.listen(port, () => {
+http.listen(port, () => {
   if (!fs.existsSync(locationParent)) {
     fs.mkdirSync(locationParent);
   }
